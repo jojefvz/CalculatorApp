c074e782fc2b56e473de93c77b74c57608d000be
Neza <112897472+jojefvz@users.noreply.github.com>
2023-02-03 16:19:52 -0600

    Sub, Mult, & Div Functions Implemented

--- calculator.py
+++ calculator.py
@@ -17,16 +17,50 @@ def button_click(number):
 def erase():
     e.delete(0, END)
 
-def plus():
-    num1 = e.get()
-    global first_num
-    first_num = int(num1)
-    e.delete(0, END)
-
 def equals():
     num2 = e.get()
     e.delete(0, END)
-    e.insert(0, first_num + int(num2))
+    if sign == '+':
+        e.insert(0, first_num + int(num2))
+    if sign == '-':
+        e.insert(0, first_num - int(num2))
+    if sign == 'x':
+        e.insert(0, first_num * int(num2))
+    if sign == '/':
+        e.insert(0, first_num / int(num2))
+
+
+def add():
+    num1 = e.get()
+    global first_num
+    global sign
+    first_num = int(num1)
+    sign = '+'
+    e.delete(0, END)
+
+def subtract():
+    num1 = e.get()
+    global first_num
+    global sign
+    first_num = int(num1)
+    sign = '-'
+    e.delete(0, END)
+
+def multiply():
+    num1 = e.get()
+    global first_num
+    global sign
+    first_num = int(num1)
+    sign = 'x'
+    e.delete(0, END)
+
+def divide():
+    num1 = e.get()
+    global first_num
+    global sign
+    first_num = int(num1)
+    sign = '/'
+    e.delete(0, END)
 
 #Define buttons
 button_1 = Button(root, text='1', padx=40, pady=20, command=lambda: button_click(1))
@@ -39,9 +73,14 @@ button_7 = Button(root, text='7', padx=40, pady=20, command=lambda: button_click
 button_8 = Button(root, text='8', padx=40, pady=20, command=lambda: button_click(8))
 button_9 = Button(root, text='9', padx=40, pady=20, command=lambda: button_click(9))
 button_0 = Button(root, text='0', padx=40, pady=20, command=lambda: button_click(0))
-button_plus = Button(root, text='+', padx=40, pady=20, command= plus)
+
 button_equals = Button(root, text='=', padx=40, pady=20, command=equals)
 button_clear = Button(root, text='Clear', padx=151, pady=20, command= erase)
+
+button_add = Button(root, text='+', padx=40, pady=20, command=add)
+button_subtract = Button(root, text='-', padx=40, pady=20, command=subtract)
+button_multiply = Button(root, text='x', padx=40, pady=20, command=multiply)
+button_divide = Button(root, text='/', padx=40, pady=20, command=divide)
 
 
 #Place butttons
@@ -58,10 +97,13 @@ button_8.grid(row=1, column=1)
 button_9.grid(row=1, column=2)
 
 button_0.grid(row=4, column=0)
-button_plus.grid(row=4, column=1)
-button_equals.grid(row=4, column=2)
+button_equals.grid(row=5, column=2)
+button_clear.grid(row=6, column=0, columnspan=3)
 
-button_clear.grid(row=5, column=0, columnspan=3)
+button_add.grid(row=4, column=1)
+button_subtract.grid(row=4, column=2)
+button_multiply.grid(row=5, column=0)
+button_divide.grid(row=5, column=1)
 
 
 root.mainloop()
